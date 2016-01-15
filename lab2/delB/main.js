3506734ceaa20cd2f6d79cd078cbde7c8a804086
Joel Edström <joel.edstrom@gmail.com>
2016-01-15 13:48:35 +0100

    Add lab2

---
+++ lab2/delB/main.js
@@ -0,0 +1,100 @@
+"use strict";
+
+
+var Game = function() {
+    
+    // PRIVATE:
+    
+    // Note: null represents the empty slot
+    var winningBoard = _.range(1, 16).concat([null]);
+    var onUpdateCallback;
+    var board;
+    
+    function swap(x, y) {
+        var tmp = board[x];
+        board[x] = board[y];
+        board[y] = tmp;
+    }
+    
+    function shuffleBoard() {
+        board = _.shuffle(winningBoard)
+    }
+    
+    function isFinished() {
+        return _.isEqual(winningBoard, board);
+    }
+    
+    return {
+        
+        // PUBLIC METHODS: 12 13 14 15
+        
+        reset: function() {
+            shuffleBoard();
+            onUpdateCallback(board);
+        },
+        
+        onUpdate: function(callback) {
+            onUpdateCallback = callback;
+        },
+        
+        performClick: function(index) {
+            if (!isFinished()) {
+                                
+                if (index > 0 && board[index - 1] === null)
+                    swap(index, index - 1);
+                else if (index < 16 && board[index + 1] === null)
+                    swap(index, index + 1);
+                else if (index > 3 && board[index - 4] === null)
+                    swap(index, index - 4);
+                else if (index < 12 && board[index + 4] === null)
+                    swap(index, index + 4);
+                else
+                    return;
+            
+                onUpdateCallback(board, isFinished());
+            }
+        },
+        
+        cheat: function() {
+            board = _.clone(winningBoard)
+            swap(14,15);
+            onUpdateCallback(board);
+        }
+    }
+}
+
+
+
+
+$(document).ready(function() {
+    
+    var game = new Game();
+     
+    $(".cell").click(function() {
+        var index = $(this).data("index");
+        game.performClick(index);
+    });
+    
+    game.onUpdate(function(board, isFinished) {
+        $(".cell").each(function(index) {
+            $(this).text(board[index] === null ? " " : board[index]);
+            $(this).data("index", index); 
+        });
+        
+        if (isFinished)
+            setTimeout(function() { alert("YOU WIN!"); });
+    })
+    
+    game.reset();
+    
+    $("#resetButton").click(function() {
+        game.reset();
+    })
+    
+    $("#cheatButton").click(function() {
+        game.cheat();
+    })
+    
+});
+
+
